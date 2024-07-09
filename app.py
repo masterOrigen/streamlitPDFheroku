be7f06015d2ae849719194b5272ad7f8d0a1f6b4
masterOrigen <167365847+masterOrigen@users.noreply.github.com>
2024-07-09 09:32:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -30,8 +30,8 @@ def get_gemini_response(prompt):
     try:
         model = genai.GenerativeModel('gemini-pro')
         response = model.generate_content(prompt, generation_config=genai.types.GenerationConfig(
-            temperature=0.3,
-            max_output_tokens=2000,
+            temperature=0.2,
+            max_output_tokens=4000,
         ))
         return response.text
     except Exception as e:
